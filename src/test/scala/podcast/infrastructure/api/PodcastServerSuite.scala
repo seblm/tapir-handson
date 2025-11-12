3e18b81758f03e81b2e65dc1016f9587d49c0446
Jerome Richard <jerome.richard@hcl-software.com>
2025-11-12 14:28:20 +0100

    step 9: expose OpenAPI with swagger-ui

--- src/test/scala/podcast/infrastructure/api/PodcastServerSuite.scala
+++ src/test/scala/podcast/infrastructure/api/PodcastServerSuite.scala
@@ -2,7 +2,7 @@ package podcast.infrastructure.api
 
 import com.github.pjfanning.pekkohttpplayjson.PlayJsonSupport
 import munit.FunSuite
-import org.apache.pekko.http.scaladsl.testkit.{MUnitRouteTest, ScalatestRouteTest}
+import org.apache.pekko.http.scaladsl.testkit.MUnitRouteTest
 
 class PodcastServerSuite extends FunSuite with MUnitRouteTest with PlayJsonSupport:
 
@@ -14,4 +14,13 @@ class PodcastServerSuite extends FunSuite with MUnitRouteTest with PlayJsonSuppo
         Some(41),
         """Please use PekkoHttpServerInterpreter to declare the Tapir endpoint implementation to pekko-http routes:
           |} ~ PekkoHttpServerInterpreter().toRoute(…)""".stripMargin
+      )
+
+  test("PodcastApi should expose OpenAPI contract with swagger-ui"):
+    Get("/docs/index.html") ~> PodcastServer.route ~> check:
+
+      import org.apache.pekko.http.scaladsl.unmarshalling.Unmarshaller.given
+      assert(
+        responseAs[String].contains("""<title>Swagger UI</title>"""),
+        "Swagger-UI can be served using `SwaggerInterpreter` with our existing endpoints, and added as a new route."
       )
